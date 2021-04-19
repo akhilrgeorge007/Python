6fc3b3517efa412540be7e6651e116fea951440d
Akhil Raymond George <akhilrgeorge007@gmail.com>
2021-04-20 02:24:03 +0530

    diamond pattern

--- pattern.py
+++ pattern.py
@@ -5,9 +5,18 @@ def x(n):
         for j in range (0,k):
             print(end=" ")
         k-=1
-        for j in range(0,i):
+        for j in range(0,i+1):
             print("*",end=" ")
         print("\r")
+    k=n
+    for i in range(0,n):
+        for j in range (0,i):
+            print(end=" ")
+     
+        for j in range(0,k):
+            print("*",end=" ")
+        k-=1
+        print("\r")  
 
 n = int(input("ENTER NUMBER ? "))
 
